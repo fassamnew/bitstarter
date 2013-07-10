1ecc4a4c8b63a97798d8d63471a7f4688cfe27fe
Fitsum Andargie <fassamnew@gmail.com>
2013-07-10 16:46:16 +0000

    modified web.js to print from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs=require('fs');
+var buf=new Buffer(fs.readFileSync("index.html"));
+var text=JSON.stringfy(buf);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
